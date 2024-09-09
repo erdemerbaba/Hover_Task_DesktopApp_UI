3de4b39e14197afdf05e1a8e2eae6a05595c6edb
ERDEM ERBABA <erdemerbaba@gmail.com>
2024-09-09 19:13:47 +0300

    Add files via upload

---
+++ project/DeleteTask.swift
@@ -0,0 +1,8 @@
+//
+//  DeleteTask.swift
+//  project
+//
+//  Created by Erdem Erbaba on 16.08.2024.
+//
+
+import Foundation
---
+++ project/projectApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .customWindowStyle()
+        }
+    }
+}
